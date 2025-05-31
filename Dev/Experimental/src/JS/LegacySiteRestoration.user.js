d4981fa9a0601630f3491795cac522c36becba3e
TersisWilvin <tanker45523@gmail.com>
2025-05-30 22:14:40 -0700

    Added Missing Files from Master.

---
+++ Dev/Experimental/src/JS/LegacySiteRestoration.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Legacy Site Restoration Experimental
+// @namespace    https://github.com/tersiswilvin
+// @version      1.3
+// @description  Restores the Roblox Site back to a familiar state.
+// @author       TersisWilvin
+// @license      CC-BY-SA-4.0
+// @match        *.roblox.com*
+// @match        *.roblox.com/*
+// @icon         https://images.rbxcdn.com/23421382939a9f4ae8bbe60dbe2a3e7e.ico.gzip
+// @updateURL    https://raw.githubusercontent.com/tersiswilvin/Roblox-2019-Old-Theme/Release/Dev/Experimental/src/JS/LegacySiteRestoration.user.js
+// @downloadURL  https://raw.githubusercontent.com/tersiswilvin/Roblox-2019-Old-Theme/Release/Dev/Experimental/src/JS/LegacySiteRestoration.user.js
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @connect      premiumfeatures.roblox.com
+// @connect      thumbnails.roblox.com
+// @connect      users.roblox.com
+// ==/UserScript==
